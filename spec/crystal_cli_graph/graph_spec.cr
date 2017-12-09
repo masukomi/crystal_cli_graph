f00a05cfb4e748152d82932b4ffad79649f3723e
masukomi <masukomi@masukomi.org>
2017-12-09 14:34:45 -0500

    supports max_width and fixed y_label

--- spec/crystal_cli_graph/graph_spec.cr
+++ spec/crystal_cli_graph/graph_spec.cr
@@ -18,11 +18,21 @@ describe CrystalCliGraph::Graph do
 
   it "should be able to generate even columns from data" do
     g = CrystalCliGraph::Graph.new(default_data, default_options)
-    columns = g.generate_columns_from_data(default_data, false, 100, nil)
+    columns = g.generate_columns_from_data(default_data, false, 100, nil, 4)
     columns_sizes = columns.map{|x|x.size}
     columns.all?{|x| x.size == 100}.should(be_true())
     columns.all?{|x| x.width == 1}.should(be_true())
-    columns.size.should(eq(7))
+  end
+
+  it "should not pad columns when there isn't space" do
+    g = CrystalCliGraph::Graph.new(default_data, default_options)
+    columns = g.generate_columns_from_data(default_data, false, 100, nil, 4)
+    columns.size.should(eq(4))
+  end
+  it "should pad columns when there is space" do
+    g = CrystalCliGraph::Graph.new(default_data, default_options)
+    columns = g.generate_columns_from_data(default_data, false, 100, nil, 16)
+    columns.size.should(eq(13)) # no padding to the right of last one.
   end
 
   it "should generate a graph with no label" do
@@ -32,6 +42,85 @@ describe CrystalCliGraph::Graph do
     matrix.size.should(eq(15)) # default max_height
     #last row (bottom) first item (left)
     matrix.last.first.should(eq(CrystalCliGraph::Graph::BAR_TOPPER))
+  end
+
+  it "should not be taller with a short y_label" do
+    opts = default_options.dup
+    opts[:y_label] = "short y_label"
+    g = CrystalCliGraph::Graph.new(default_data, opts)
+    output = g.generate
+    matrix = output.split("\n").map{|row| row.split("")}
+    matrix.size.should(eq(15)) # default max_height
+  end
+  it "should not be taller with an equally sized y_label" do
+    opts = default_options.dup
+    opts[:y_label] = "a short y_label" # 15 chars
+    g = CrystalCliGraph::Graph.new(default_data, opts)
+    output = g.generate
+    matrix = output.split("\n").map{|row| row.split("")}
+    matrix.size.should(eq(15)) # default max_height
+  end
+  it "should be taller with a tall y_label" do
+    opts = default_options.dup
+    opts[:y_label] = "this y_label is like really tall" # 32 chars
+    g = CrystalCliGraph::Graph.new(default_data, opts)
+    output = g.generate
+    matrix = output.split("\n").map{|row| row.split("")}
+    matrix.size.should(eq(32)) # default max_height
+  end
+
+  it "should render the y_label vertically in the 1st column" do
+    opts = default_options.dup
+    opts[:y_label] = "this y_label is like really tall" # 32 chars
+    g = CrystalCliGraph::Graph.new(default_data, opts)
+    output = g.generate
+    matrix = output.split("\n").map{|row| row.split("")}
+    reconstituted_arr = Array(String).new
+    (0...matrix.size).each do |idx|
+      reconstituted_arr.push matrix[idx][0] 
+    end
+    reconstituted_arr.join("").should(eq(opts[:y_label]))
+  end
+  it "should render the y_label vertically in the 1st column with padding" do
+    opts = default_options.dup
+    opts[:y_label] = "this" # 32 chars
+    g = CrystalCliGraph::Graph.new(default_data, opts)
+    output = g.generate
+    matrix = output.split("\n").map{|row| row.split("")}
+    reconstituted_arr = Array(String).new
+    (0...matrix.size).each do |idx|
+      char = matrix[idx][0]
+      reconstituted_arr.push char unless char == " "
+    end
+    reconstituted_arr.join("").should(eq(opts[:y_label]))
+    matrix[matrix.size - 1][0].should(eq("s")) # rendered vertically, last char
+  end
+
+
+  it "should calculate padding columns when max_width = data size" do
+    opts = default_options.dup
+    opts[:max_width] = 5
+    g = CrystalCliGraph::Graph.new(default_data, opts)
+    padding_columns = g.get_padding_columns_count(default_data,
+                                                  opts[:max_width].as(Int32))
+    padding_columns.should(eq(0))
+  end
+  
+  it "should calculate padding columns when max_width < data size" do
+    opts = default_options.dup
+    opts[:max_width] = 1
+    g = CrystalCliGraph::Graph.new(default_data, opts)
+    padding_columns = g.get_padding_columns_count(default_data, opts[:max_width].as(Int32))
+    padding_columns.should(eq(0))
+  end
+  it "should calculate padding columns when max_width < data size" do
+    opts = default_options.dup
+    data = [0,1,2,3,4,5] # 6 wide so won't divide evenly
+    opts[:max_width] = 20
+    g = CrystalCliGraph::Graph.new(data, opts)
+    padding_columns = g.get_padding_columns_count(data, opts[:max_width].as(Int32))
+    padding_columns.should(eq(2))
+    # 1 data column + 2 padding columns * 6 = 18
   end
 end
 
--- src/crystal_cli_graph/graph.cr
+++ src/crystal_cli_graph/graph.cr
@@ -5,17 +5,18 @@ module CrystalCliGraph
     getter :data
     BAR_TOPPER="o"
     @columns : Array(Column)
-    def initialize(@data : Array(Int32), options : Hash(Symbol, Bool|Int32|String))
-      @fit_min = options.has_key?(:fit_min) ? options[:fit_min].as(Bool) : false
-      @max_height = options.has_key?(:max_height) ?  options[:max_height].as(Int32) : 15
-      @x_label = options.has_key?(:x_label) ? options[:x_label].as(String?) : nil
-      @y_label = options.has_key?(:y_label) ? options[:y_label].as(String?) : nil
-      @min_width = options.has_key?(:min_width)
+    def initialize(@data : Array(Int32), options : Hash(Symbol, Bool|Int32|String?))
+      @fit_min = options.fetch(:fit_min, false).as(Bool)
+      @max_height = options.fetch(:max_height, 15).as(Int32)
+      @x_label = options.fetch(:x_label, nil).as(String?)
+      @y_label = options.fetch(:y_label, nil).as(String?)
+      @max_width = options.fetch(:max_width, @data.size).as(Int32)
+      # max_width WILL be exceeded if there are more data elements than it
       # we need to take 
       # [0, 1, 15, 20, 2]
       # and convert it into height bars.
       @columns = generate_columns_from_data(@data, @fit_min, @max_height,
-                                            @y_label)
+                                            @y_label, @max_width)
     end
     def generate
       t = Table.new(@columns)
@@ -26,21 +27,40 @@ module CrystalCliGraph
       options[:show_header] = false
       t.format(options)
     end
+
+    def get_padding_columns_count(data : Array(Int32), max_width : Int32) : Int32
+      datums = data.size
+      if datums >= max_width
+        return 0
+      else
+        padding = (max_width / data.size) - 1
+        return (padding > 0 ? padding : 0)
+      end
+    end
+    
     def generate_columns_from_data(data : Array(Int32), fit_min : Bool,
                                         max_height : Int32,
-                                        y_label : String?)
+                                        y_label : String?,
+                                        max_width : Int32)
       # we have max_height + 1 possible different bars (+1 for zero height)
       # each bar being able to have 1 step towards max_height
-      columns = Array(Column).new(data.size)
+      y_label_columns = y_label.nil? ? Array(Column).new : generate_y_label_columns(max_height, y_label.to_s)
+      pcc = get_padding_columns_count(data, max_width)
+      total_cols = data.size + (y_label.nil? ? 0 : y_label_columns.size) + ((data.size * pcc) - pcc)
+      
+      columns = Array(Column).new(total_cols)
+      if !y_label.nil?
+        max_height = y_label_columns.first.size
+        columns += y_label_columns
+      end
+
+
       min_range = fit_min ? data.min : 0
       bar_range = data.max - min_range
       bar = bar_range.to_f / (max_height - 1).to_f
       bar = 1 if bar < 1
       
-      if !y_label.nil?
-        y_label_columns = generate_y_label_columns(max_height, y_label.to_s)
-        columns += y_label_columns
-      end
+      padding_column = Column.new(Array(String|Nil).new()+(" " * max_height).split(""))
       data.each_with_index do |int, idx|
         bar_height = ((int - min_range) / bar).to_i
         padding_needed = max_height - bar_height # possibly higher based on label
@@ -53,15 +73,16 @@ module CrystalCliGraph
           ("|" * ( bar_height > 0 ? (bar_height - 1) : 0 ))).split("")
         # find a more efficient way to do that^^
         columns.push(Column.new(column_data))
-        if (idx < data.size - 1)
-        columns.push(Column.new(Array(String|Nil).new()+(" " *
-                                                   column_data.size).split("")))
+        if (idx < data.size - 1 && pcc > 0)
+          pcc.times do 
+            columns.push(padding_column)
+          end
         end
       end
       columns
     end
     
-    private def generate_y_label_columns(max_height : Int32, y_label : String)
+    private def generate_y_label_columns(max_height : Int32, y_label : String) : Array(Column)
       col_data = Array(String|Nil).new()
       if y_label.size >= max_height
         max_height = y_label.size
