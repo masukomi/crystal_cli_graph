331f4fa5a335f03907f8f97cd9977ee8f2e72170
masukomi <masukomi@masukomi.org>
2018-01-03 20:50:02 -0500

    added CrystalCliGraph#get_key_chars to support custom keys

--- src/crystal_cli_graph.cr
+++ src/crystal_cli_graph.cr
@@ -7,6 +7,19 @@ module CrystalCliGraph
     (data.size / key_chars.size) + 1
   end
 
+  def self.get_key_chars(custom_chars : Array(String)?) : Array(String)
+    if ! custom_chars.nil?
+      cc = custom_chars.as(Array(String))
+      if ! cc.empty?
+        cc
+      else
+        KEY_CHARS
+      end
+    else
+      KEY_CHARS
+    end
+  end
+
   def self.generate_key_for_index(idx : Int32, max_key_chars : Int32,
                                   key_chars : Array(String)) : String
     multiplier = (idx / key_chars.size) + 1
